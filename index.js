17e1e91dd0f9ee98d93c5f20fe800fec46028cd9
Jhmeel <muhammadjhmeel01@gmail.com>
2024-06-21 02:23:08 +0100

    changes

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import http from 'http';
 import { Server } from 'socket.io';
 import multer from 'multer';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import fs from 'fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
